4a8766b716ec848a417f99b5cb9837099b76b84b
unnikottamam <unnikottamam@gmail.com>
2023-10-27 21:32:55 -0700

    Testing commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Leads CRM</h1>
-          <p className="py-6">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+          <p className="py-6">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt ame
             fugiat veniam occaecat fugiat aliqua.</p>
           <Button link="/leads" className="btn-xs">See All Lead</Button>
         </div>
